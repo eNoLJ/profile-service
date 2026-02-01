cb94bd47728c500e4c319a81db9f7df8ba53dc15
eNoLJ <derosatam76@gmail.com>
2026-02-01 15:58:17 +0900

    feat: 컨트롤러 요청 시 로그 추가

--- src/main/java/org/enolj/profileservice/controller/MemberController.java
+++ src/main/java/org/enolj/profileservice/controller/MemberController.java
@@ -2,6 +2,7 @@ package org.enolj.profileservice.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.enolj.profileservice.dto.GetMemberResponse;
 import org.enolj.profileservice.dto.SaveMemberRequest;
 import org.enolj.profileservice.dto.SaveMemberResponse;
@@ -13,18 +14,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/members")
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
 
     @PostMapping
     public ResponseEntity<SaveMemberResponse> saveMember(@Valid @RequestBody SaveMemberRequest request) {
+        log.info("[API - LOG] 회원 저장 컨트롤러");
         SaveMemberResponse response = memberService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping("/{memberId}")
     public ResponseEntity<GetMemberResponse> getMember(@PathVariable Long memberId) {
+        log.info("[API - LOG] 회원 조회 컨트롤러");
         GetMemberResponse response = memberService.getMember(memberId);
         return ResponseEntity.ok(response);
     }
--- src/main/java/org/enolj/profileservice/service/MemberService.java
+++ src/main/java/org/enolj/profileservice/service/MemberService.java
@@ -1,6 +1,7 @@
 package org.enolj.profileservice.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.enolj.profileservice.dto.GetMemberResponse;
 import org.enolj.profileservice.dto.SaveMemberRequest;
 import org.enolj.profileservice.dto.SaveMemberResponse;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -22,9 +24,13 @@ public class MemberService {
         return  SaveMemberResponse.from(member);
     }
 
+    @Transactional(readOnly = true)
     public GetMemberResponse getMember(Long memberId) {
         Member member = memberRepository.findById(memberId).orElseThrow(
-                () -> new IllegalArgumentException("해당 멤버를 찾을 수 없습니다.")
+                () -> {
+                    log.error("Member 조회 실패 - memberId={}", memberId);
+                    return new IllegalArgumentException("해당 멤버를 찾을 수 없습니다.");
+                }
         );
         return GetMemberResponse.from(member);
     }
